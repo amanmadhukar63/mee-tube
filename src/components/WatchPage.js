7e45626b74c5944a0f98dd2be33d537cf333493a
amanmadhukar63 <amanmadhukar63@gmail.com>
2023-09-28 22:10:17 +0530

    watch page setup

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,23 +1,72 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux'
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import { videoAPI, relatedVideoAPI } from '../utils/constant';
 
 const WatchPage = () => {
     const [params]=useSearchParams();
     const id=params.get('v');
     const dispatch=useDispatch();
+    const [videoDetail,setVideoDetail]=useState({});
+    const [relatedVideo,setRelatedVideo]=useState([]);
+
     useEffect(()=>{
         dispatch(closeMenu());
+        getVideoData();
+        getReltedVideo();
     },[]);
+
+    const getVideoData=async()=>{
+      const data=await fetch(videoAPI+id);
+      const json=await data.json();
+      setVideoDetail(json.items[0]);
+    }
+
+    const getReltedVideo=async()=>{
+      const data=await fetch(relatedVideoAPI+id);
+      const json=await data.json();
+      console.log(json);
+    }
+
+    function convert (labelValue) {
+
+      return Math.abs(Number(labelValue)) >= 1.0e+9
+  
+      ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
+      : Math.abs(Number(labelValue)) >= 1.0e+6
+  
+      ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1) + "M"
+      : Math.abs(Number(labelValue)) >= 1.0e+3
+  
+      ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(0) + "K"
+  
+      : Math.abs(Number(labelValue));
+  
+  }
+
+    if(Object.keys(videoDetail).length === 0) return null;
+
+    const {title,channelTitle}=videoDetail.snippet;
+    const {viewCount,likeCount}=videoDetail.statistics;
+
+
   return (
-    <div>
+    <div className='mx-5 p-2' >
       <iframe
-      className='mx-5 p-2' 
-      width="900" 
+      width="950" 
       height="500" 
-      src={"https://www.youtube.com/embed/"+id} 
+      src={"https://www.youtube.com/embed/"+id+"?autoplay=1"} 
       title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      
+      <p className='font-semibold text-[22px] py-3'>{title}</p>
+      <div className='flex justify-between'>
+        <p className='font-semibold text-xl'>{channelTitle}</p>
+        <div className='flex justify-around w-1/2'>
+          <p className='font-normal text-lg'>{convert(viewCount)} views</p>
+          <p className='font-normal text-lg'>{convert(likeCount)} likes</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,6 +4,13 @@ const API =
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=48&key="+ApiKey;
 
 export const searchAPI=
-"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key="+ ApiKey +"&q=";
+"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video,channel&key="+ ApiKey +"&q=";
+
+export const videoAPI=
+"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key="+ApiKey+"&id=";
+
+export const relatedVideoAPI=
+"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&key="+ApiKey+"&relatedToVideoId=";
+
 
 export default API;
